b3551ddfd27e5307dec03390f30e24f702c5fdb9
gegorov <gegorov@yahoo.com>
2018-09-25 03:08:11 +0300

    Zeros: solution added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
   // your implementation
-}
+  let count = 0;
+  let num = number;
+  while (num >= 5) {
+    num = Math.floor(num / 5);
+    count = count + num;
+  }
+  return count;
+};
